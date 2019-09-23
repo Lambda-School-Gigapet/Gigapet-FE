6f72b8699813d02711653567a728d4b538644324
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-23 16:07:00 -0700

    change input from email to username + style form

--- gigapet/src/components/Forms/Login.jsx
+++ gigapet/src/components/Forms/Login.jsx
@@ -1,13 +1,31 @@
 import React from "react"
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { Form, Button } from 'semantic-ui-react'
+import styled from 'styled-components'
 
 import useForm from '../../hooks/useForm'
+
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+
+const Underlined = styled.span`
+	text-decoration: underline
+`	
+
+const P = styled.p`
+	margin: 1rem 0;
+`
+
 
 export default function Login(props) {
 	// TODO: default the email value based on credentials stored on global context object 
   	const initialState = {
-		email: '',
+		username: '',
 		password: ''
 	}
 
@@ -24,36 +42,37 @@ export default function Login(props) {
 	const [loginCredentials, handleChanges, handleSubmit] = useForm(initialState, handleSubmitCb)
   
 	return (
-		<>
-		<h2>Login</h2>
-		<form onSubmit={handleSubmit}>
-			<label>
-				Email: 
-				<input 
-					name="email" 
-					type="email"
-					placeholder="someone@example.com"
-					value={loginCredentials.email}
-					onChange={handleChanges}
-				/>
-			</label>
+		<Container>
+			<h2>Login</h2>
+			<Form onSubmit={handleSubmit}>
+				<Form.Field>
+					<label>Username</label>
+					<input
+						name="username" 
+						type="text"
+						placeholder="enter your username"
+						value={loginCredentials.username}
+						onChange={handleChanges}
+					/>
+				</Form.Field>
+				
+				<Form.Field>
+					<label>Password</label>
+					<input 
+						name="password" 
+						type="password"
+						placeholder="enter your password"
+						value={loginCredentials.password}
+						onChange={handleChanges}
+					/>
+				</Form.Field>
+				<Button type="submit">Login</Button>
+			</Form>
 
-			<label>
-				Password:
-				<input 
-					name="password" 
-					type="password"
-					placeholder="enter your password"
-					value={loginCredentials.password}
-					onChange={handleChanges}
-				/>
-			</label>
-
-			<button type="submit">Login</button>
-		</form>
-
-		<p>Not a user? <Link to="/register">Register now.</Link></p>
-		</>
+			<P>
+				<strong>Not a user?</strong> <Link to="/register"><Underlined>Register now.</Underlined></Link>
+			</P>
+		</Container>
 	)
 }
 
