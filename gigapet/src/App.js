3604a1418cc3c2390340c9602150f3c929c2b3f3
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-23 13:22:05 -0700

    render index, login, and register routes

--- gigapet/src/App.js
+++ gigapet/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import './App.css'
+import RegisterForm from './components/Forms/Register'
+import LoginForm from './components/Forms/Login'
 
 export default function App() {
   return (
     <div className="App">
         <h2>Gigapet</h2>
+        {/* TODO: render naviagtion and footer components  */}
+        <Route exact path="/" component={LoginForm} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/register" component={RegisterForm} />
     </div>
   )
 }
