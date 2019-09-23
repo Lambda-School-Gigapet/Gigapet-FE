93247dc7710a0a9afbdeea2d26d934587f9d19a7
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-23 14:08:53 -0700

    render the Dashboard as a ProtectedRoute

--- gigapet/src/App.js
+++ gigapet/src/App.js
@@ -1,19 +1,25 @@
+// npm packages
 import React from 'react'
 import { Route } from 'react-router-dom'
+// Styles 
 import './App.css'
+// Components
 import RegisterForm from './components/Forms/Register'
 import LoginForm from './components/Forms/Login'
+import Dashboard from './components/Dashboard'
+import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
-  return (
-    <div className="App">
-        <h2>Gigapet</h2>
-        {/* TODO: render naviagtion and footer components  */}
-        <Route exact path="/" component={LoginForm} />
-        <Route path="/login" component={LoginForm} />
-        <Route path="/register" component={RegisterForm} />
-    </div>
-  )
+	return (
+		<div className="App">
+			<h2>Gigapet</h2>
+			{/* TODO: render naviagtion and footer components  */}
+			<Route exact path="/" component={LoginForm} />
+			<Route path="/login" component={LoginForm} />
+			<Route path="/register" component={RegisterForm} />
+			<ProtectedRoute path="/dashboard" component={Dashboard}/>
+		</div>
+	)
 }
 
 
