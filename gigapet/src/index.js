3d76c90b3ff127397f43b560a0c1c2bf4fedd058
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-23 14:12:18 -0700

    set up and connect Redux store

--- gigapet/src/index.js
+++ gigapet/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import './index.css'
 import App from './App'
+import store from './store'
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 )
---
+++ gigapet/src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reduxThunk from 'redux-thunk'
+
+import rootReducer from './reducers'
+
+export default createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(reduxThunk))
+)
