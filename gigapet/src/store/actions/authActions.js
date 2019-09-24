1e080e9b9594b20ef5ae89b6305c91771ff74eeb
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-24 10:50:21 -0700

    add actions and an AC for user registration

---
+++ gigapet/src/store/actions/authActions.js
@@ -0,0 +1,21 @@
+import { pick } from 'ramda'
+import axios from 'axios'
+
+
+export const REGISTER_START = "REGISTER_START"
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS"
+export const REGISTER_FAILURE = "REGISTER_FAILURE"
+
+export const registerUser = (newUserCredentials, history) => dispatch => {
+    dispatch({ type: REGISTER_START })
+    axios.post('https://gigapets-be.herokuapp.com/api/auth/register', 
+                   pick(['email', 'username', 'password'], newUserCredentials)
+    )
+    .then(res => {
+        // notify user of success processing their input
+        // TODO: dispatch username to the store
+        console.log('Register success')
+        history.push('/login')
+    })
+    .catch(console.error)
+}
---
+++ gigapet/src/store/actions/index.js
@@ -0,0 +1,13 @@
+import { 
+    REGISTER_START,
+    REGISTER_SUCCESS,
+    REGISTER_FAILURE,
+    registerUser
+} from './authActions'
+
+export {
+    REGISTER_START,
+    REGISTER_SUCCESS,
+    REGISTER_FAILURE,
+    registerUser 
+}
