8fbd247c30b0b640ba6702b841f1463ae151c0a0
dAVE Inden <davidinden@gmail.com>
2019-09-25 10:45:08 -0700

    handle empty array for list of kids to note user should add kids

--- gigapet/src/components/Children/Children.jsx
+++ gigapet/src/components/Children/Children.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { Card } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 //Helper util to request data with auth token of user
 import axiosWithAuth from '../../utils/axiosWithAuth';
@@ -28,39 +29,49 @@ export default function Children(props) {
 
     //Set state of the current children for tracking what to display
     //initializd with a empty array while data loads
-    const [ children, setChildren ] = useState([])
+    const [ children, setChildren ] = useState(null)
 
-    const userId = 1;
+    const { userId } = useSelector(state => ({ userId: state.user.id }));
 
     //Axios request for data on children
     //Dep array tracks if the data for the children has changed
     useEffect(() => {
-        // axiosWithAuth(`/api/${userId}/kids`)
-        // //TODO: need endpoint for getting children data
-        // .get('')
-        // .then(res => {
-        //     //Request data on the children and set the state of the list of children to the response data
-        //     setChildren(res.data)
-        // }).catch(err => {
-        //     //Log error response to console for now until error handling is decided
-        //     console.log('There was an error getting the children data', err)
-        // })
-        setTimeout(()=>{
-            setChildren(mockData)
-        }, 1000)
-    }, [children])
+        axiosWithAuth()
+        //TODO: need endpoint for getting children data
+        .get(`/${userId}/kids`)
+        .then(res => {
+            //Request data on the children and set the state of the list of children to the response data
+            setChildren(res.data)
+            console.log(res.data)
+        }).catch(err => {
+            //Log error response to console for now until error handling is decided
+            console.log('There was an error getting the children data', err)
+        })
+        // setTimeout(()=>{
+        //     setChildren(mockData)
+        // }, 1000)
+    }, [userId])
 
-    return (
-        children.length === 0 ? 
-        <ChildrenContainer>
-            <h2>Your Children: </h2>
-            <Card header="Loading..."/>
-        </ChildrenContainer> :
-        <>
+    if (!children) {
+        return (
+            <ChildrenContainer>
+                <h2>Your Children: </h2>
+                <Card header="Loading..."/>
+            </ChildrenContainer>
+        )
+    } else if (children.length === 0) {
+        return (
+            <ChildrenContainer>
+                <h2>Your Children: </h2>
+                <Card header="No Kids in your list. Add new Kids"/>
+            </ChildrenContainer>
+        )
+    } else {
+        return (
             <ChildrenContainer>
                 <h2>Your Children: </h2>
                 {children.map((child, idx) => <CardContainer key={idx}><Link to={`/child-dashboard/${child.id}`}><Child name={child.name} age={child.age} weight={child.weight} /></Link></CardContainer>)}
             </ChildrenContainer>
-        </>
-    )
+        )
+    }
 }
