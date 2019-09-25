52fff7a3a780f418a91973546ba2b682f721e405
dAVE Inden <davidinden@gmail.com>
2019-09-25 08:04:02 -0700

    remove styled-components from meal

--- gigapet/src/components/RecentMealsAllChildren/Meal.jsx
+++ gigapet/src/components/RecentMealsAllChildren/Meal.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Card } from 'semantic-ui-react'
 
-//Container div to display data on a single meal
-// const MealContainer = styled.div`
-//     width: 100%;
-//     display: flex;
-//     justify-content: space-between;
-//     align-content: center;
-//     border-top: 2px solid black;
-//     padding: 5px;
-// `
 
 //Meal container is returned with data for the specific meal from props
 export default function Meal(props) {
