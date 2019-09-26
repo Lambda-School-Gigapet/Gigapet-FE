8331050319d45cc565d680c27408007abd415e1a
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-25 17:58:16 -0700

    add dropdown choices to NewFoodEntry form

--- gigapet/src/components/Forms/NewFoodEntry.jsx
+++ gigapet/src/components/Forms/NewFoodEntry.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useDispatch } from 'react-redux'
-import { Modal, Button, Form } from 'semantic-ui-react'
+import { Modal, Button, Form, Dropdown } from 'semantic-ui-react'
 import styled from 'styled-components'
-import { assoc, compose, evolve } from 'ramda'
+import * as R from 'ramda'
 
 import useForm from '../../hooks/useForm'
 import { addNewMealEntry } from '../../store/actions'
@@ -22,11 +22,11 @@ export default function NewFoodEntry(props) {
     
     const initialStateNewMealEntry = {
         kids_id: props.childId,
-        date: '', // "09-12-2019",
-        meal: '', // "lunch",
-        food:  '', //"cheese",
-        category: '', // "dairy",
-        servings: '' // 2
+        date: '', 
+        meal: '', 
+        food:  '',
+        category: '',
+        servings: '' 
     }
 
     const formatDate = date => {
@@ -36,28 +36,90 @@ export default function NewFoodEntry(props) {
     }
     
     const handleSubmitCb = newMealEntry => {
-        compose(
+        
+        R.compose(
             dispatch,
             addNewMealEntry.bind(null, newMealEntry.kids_id),
-            evolve({
+            R.evolve({
+                meal: R.always(mealChoice.current.state.value),
+                category: R.always(foodCategoryChoice.current.state.value),
                 kids_id: parseInt,
                 servings: parseInt,
                 date: formatDate
             })
         )(newMealEntry)   
         
+        // reset the form 
         setNewMealEntry(initialStateNewMealEntry)
     }
     
     const [newMealEntry, setNewMealEntry, handleChanges, handleSubmit] = useForm(initialStateNewMealEntry, handleSubmitCb)
     const [modalOpen, setModalOpen] = useState(false)
+    const foodCategoryChoice = useRef()
+    const mealChoice = useRef()
 
     const handleOpen = () => setModalOpen(true)
     // The form won't submit without this setTimeout hack.
     // Without the setTimeout, the following warning appears in the console: 
     // Form submission canceled because the form is not connected
     const handleClose = () => setTimeout(() => setModalOpen(false), 0)
-    
+
+    const mealOptions = [
+        {
+            key: 'Breakfast',
+            text: 'Breakfast',
+            value: 'Breakfast'  
+        },
+        {
+            key: 'Lunch',
+            text: 'Lunch',
+            value: 'Lunch'  
+        },
+        {
+            key: 'Dinner',
+            text: 'Dinner',
+            value: 'Dinner'  
+        }
+    ]
+
+    const foodCategories = [
+        {
+            key: 'Treats',
+            text: 'Treats',
+            value: 'Treats'
+        },
+        {
+            key: 'Fruit',
+            text: 'Fruit',
+            value: 'Fruit'
+        },
+        {
+            key: 'Veggies',
+            text: 'Veggies',
+            value: 'Veggies'
+        },
+        {
+            key: 'Dairy',
+            text: 'Dairy',
+            value: 'Dairy'
+        },
+        {
+            key: 'Grains/ Beans',
+            text: 'Grains/ Beans',
+            value: 'Grains/ Beans'
+        },
+        {
+            key: 'Meat/ Poultry/ Seafood',
+            text: 'Meat/ Poultry/ Seafood',
+            value: 'Meat/ Poultry/ Seafood'
+        },
+        {
+            key: 'Fats/ Oils',
+            text: 'Fats/ Oils',
+            value: 'Fats/ Oils'
+        }
+    ]
+
     return (
         <Modal 
             trigger={
@@ -84,12 +146,13 @@ export default function NewFoodEntry(props) {
                             />
                         </Form.Field>
                         <Form.Field>
-                            <label>Meal (Breakfast/ Lunch/ Dinner)</label>
-                            <input 
-                                name="meal"
-                                type="text"
-                                value={newMealEntry.meal}
-                                onChange={handleChanges}
+                            <label>Meal</label>
+                            <Dropdown 
+                                placeholder='Choose one'
+                                selection
+                                fluid
+                                options={mealOptions}
+                                ref={mealChoice}
                             />
                         </Form.Field>
                         <Form.Field>
@@ -103,11 +166,12 @@ export default function NewFoodEntry(props) {
                         </Form.Field>
                         <Form.Field>
                             <label>Category</label> 
-                            <input 
-                                name="category"
-                                type="text" 
-                                value={newMealEntry.category}
-                                onChange={handleChanges}
+                            <Dropdown 
+                                placeholder='Choose one'
+                                selection
+                                fluid
+                                options={foodCategories}
+                                ref={foodCategoryChoice}
                             />
                         </Form.Field>
                         <Form.Field>
