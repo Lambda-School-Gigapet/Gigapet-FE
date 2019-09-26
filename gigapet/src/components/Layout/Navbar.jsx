0b44d33e68413c198762831cfadf6429cdbec5c7
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-26 15:26:00 -0700

    style NavLinks
    Co-authored-by: Roy Wakumelo <roywakumelojr@gmail.com>
    Co-authored-by: Dave Inden <davidinden@gmail.com>

--- gigapet/src/components/Layout/Navbar.jsx
+++ gigapet/src/components/Layout/Navbar.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
+import styled from 'styled-components'
+
+const BlackAndCursive = styled.div`
+    @import url('https://fonts.googleapis.com/css?family=Lobster&display=swap');
+    font-family: Lobster, cursive;
+
+    a {
+        color: black;
+    }
+`
 
 export default function NavBar() {
     return (
         <Menu>
             <Menu.Item className="menu-items">
-                <NavLink to="/dashboard">Dashboard</NavLink>
+                <BlackAndCursive><NavLink to="/dashboard">Dashboard</NavLink></BlackAndCursive>
             </Menu.Item>
             
             <Menu.Item>
-                <NavLink to="/settings">Settings</NavLink>
+            <BlackAndCursive><NavLink to="/settings">Settings</NavLink></BlackAndCursive>
             </Menu.Item>
         </Menu>
     )
